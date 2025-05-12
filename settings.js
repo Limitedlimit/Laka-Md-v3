7bc6eb1c29c55df21f95e1443d1ab4d1bc686c98
Limitedlimit <limitmax12@gmail.com>
2025-05-12 19:21:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=4xIShSpK#5-8U7gq5_f_K4m-Cs6m7Q7NgliCuFZLKtRvaEjz7oUc",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
